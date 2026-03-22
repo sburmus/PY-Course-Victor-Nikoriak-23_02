0bb5bee5c8ff8c3c406ec2453c432027f52c6b94
Svitlana Burmus <svitlanaburmus@gmail.com>
2026-03-23 00:10:24 +0200

    task2_done

--- assignments/project_HW 09_task2/modul_1.py
+++ assignments/project_HW 09_task2/modul_1.py
@@ -1,23 +1,15 @@
-import tax_09  
+def calculate():
+    try:
+        a = float(input("Введіть число a: "))
+        b = float(input("Введіть число b: "))
+        
+        result = (a ** 2) / b
+        print(f"Результат: {result}")
+        return result
 
-def ask_name():
-    user_name = input("Як ваше ім'я? ")
-    return user_name
+    except ValueError:
+        print("Помилка: потрібно вводити лише числа.")
+    except ZeroDivisionError:
+        print("Помилка: не можна ділити на нуль.")
 
-def total_price():
-    user_amount = float(input("Яка сума покупки (€)? "))
-    return user_amount
-
-def main():
-    name = ask_name()
-    amount = total_price()
-    tax_amount = tax_09.import_tax(amount)
-    final_amount = amount + tax_amount
-
-    print(f"Hello, {name}!")
-    print(f"{name}, ваша сума покупки: {amount:.2f} €")
-    print(f"Податок: {tax_amount:.2f} €")
-    print(f"Сума з податком: {final_amount:.2f} €")
-
-if __name__ == "__main__":
-    main()
+calculate()
