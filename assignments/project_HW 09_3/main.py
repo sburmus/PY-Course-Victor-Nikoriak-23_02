c372f3397c9727173a255942cefc89b1ceff7516
Svitlana Burmus <svitlanaburmus@gmail.com>
2026-04-15 18:57:53 +0300

    Виправлено порядок викликів функцій у основному файлі, закоментовано старі виклики для підрахунку рядків та символів.

--- assignments/project_HW 09_3/main.py
+++ assignments/project_HW 09_3/main.py
@@ -7,5 +7,6 @@ def main():
 if __name__ == "__main__":
  main()
 
-print(mymod.count_lines("sample.txt"))
-print(mymod.count_chars("sample.txt"))
+#print(mymod.count_lines("sample.txt"))
+#print(mymod.count_chars("sample.txt"))
+mymod.test("sample.txt")
